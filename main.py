8da00d5bbc550e9cf3b45de6f1668e7fb5d01b46
christoharris <142194740+christoharris@users.noreply.github.com>
2023-12-12 16:24:34 -0700

    Update main.py
    cleaned up code, extensive comments to help understand what exactly is going on with each line. an AI helped me get a prototype going a few months ago, now its time to take this shit to the next level.
    
    What this really represents is the first of many functions to be built. It is a simple squared symmetry transformation of the original inputted image.

--- main.py
+++ main.py
@@ -5,47 +5,47 @@ import streamlit as st
 st.set_page_config(page_title='Four times')
 st.title('Four times')
 
-
-
 def sqaure(upload):
+
+    # initiate image variable
     image1 = upload
+
+    # flip image horizontally
     image2 = image1.transpose(Image.FLIP_LEFT_RIGHT)
-    base_height = min(image1.height, image2.height)
-    image1 = image1.resize((int((base_height / image1.height) * image1.width), base_height))
-    image2 = image2.resize((int((base_height / image2.height) * image2.width), base_height))
-    combined = Image.new("RGB", (image1.width + image2.width, base_height))
+    
+    # creates a canvas with dimensional integrity and stores into variable 'combined'
+    combined = Image.new("RGB", (image1.width + image2.width, image1.height))
+
+    # pastes the image into the canvas at the origin
     combined.paste(image1, (0, 0))
+
+    # pastes the reflected image into the canvas
     combined.paste(image2, (image1.width, 0))
 
-    # flip combined and paste it below
+    # flips the canvas 'combined' and stores the vertical transformation into the variable flipped_combined
     flipped_combined = combined.transpose(Image.FLIP_TOP_BOTTOM)
+
+    # creates a canvas for the vertical transformation and orignal combined variable to be pasted into
     new_combined = Image.new("RGB", (combined.width, combined.height * 2))
 
     # Paste the original combined image at the top
     new_combined.paste(combined, (0, 0))
 
-    # Paste the flipped image at the bottom
+    # Paste the vertical transformation at the bottom of new canvas, final four square product.
     new_combined.paste(flipped_combined, (0, combined.height))
 
-    # Save the final image
     return new_combined
         
-
-    # combined.save("combined.jpg")
-    # combined.save("combined.jpg")
-
 
 # file input
 file = st.file_uploader('Enter Image', type=['jpg', 'png'])
 
 
 if file is not None:
-    # Open the image file
+
     image = Image.open(file)
-    
-    # Display image
-    # st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+
     four_times = sqaure(image)
-
-
     st.image(four_times, caption='Uploaded Image', use_column_width=True)
+
