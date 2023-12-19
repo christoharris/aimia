7aa8a66a791fdb8a26713cf5f936d0c575ee3d95
christoharris <142194740+christoharris@users.noreply.github.com>
2023-12-19 13:33:52 -0700

    Update main.py
    added input an image via camera

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ import streamlit as st
 st.set_page_config(page_title='Four times')
 st.title('Four times')
 
+st.write('upload an image or take a picture')
+
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+# To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
+# Check the type of bytes_data:
+# Should output: <class 'bytes'>
+    st.write(type(bytes_data))
+
 def sqaure(upload):
 
     # initiate image variable
